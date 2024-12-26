64009bce02079e9eb8f9d15e964003f48bab546b
Ariel-Vargas <arielvargas2004@hotmail.com>
2024-12-26 13:33:58 -0500

    movil app

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,67 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, signal } from '@angular/core';
+import { 
+  /* Importe los componentes de la UI */
+  IonCardContent, IonButton, IonList, IonItem, IonLabel,
+  IonFab, IonFabButton, IonIcon, IonCard,
+  IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+/* Importe la función y el ícono */
+import { addIcons } from 'ionicons';
+import { cloudUploadOutline } from 'ionicons/icons';
+
+import { TeachablemachineService } from '../services/teachablemachine.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    /* Registre los componentes de la UI */
+    IonCardContent, IonButton, IonList, IonItem, IonLabel,
+    IonFab, IonFabButton, IonIcon, IonCard,
+    IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
-  constructor() {}
+
+  imageReady = signal(false)
+  imageUrl = signal("")
+
+  /* Declare los atributos para almacenar el modelo y la lista de clases */
+  modelLoaded = signal(false);
+  classLabels: string[] = [];
+
+
+  constructor(private teachablemachine: TeachablemachineService) {
+    /* Registre el ícono */
+    addIcons({ cloudUploadOutline });
+  }
+
+  /* El método onSubmit para enviar los datos del formulario mediante el servicio */
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+        const file = input.files[0];
+   
+        const reader = new FileReader();
+
+        // Convertir el archivo a una URL base64 para mostrarlo en el html
+        reader.onload = () => {
+          this.imageUrl.set(reader.result as string)
+          this.imageReady.set(true)
+      };
+
+        reader.readAsDataURL(file); // Leer el archivo como base64
+    }
+  }
+
+  /* Método ngOnInit para cargar el modelo y las clases */
+  async ngOnInit() {
+      await this.teachablemachine.loadModel()
+      this.classLabels = this.teachablemachine.getClassLabels()
+      this.modelLoaded.set(true)
+  }
+
 }
